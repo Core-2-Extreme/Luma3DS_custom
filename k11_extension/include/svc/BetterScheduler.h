dcf03d39fb0e8c1cdbb07b144d63de567a958045
Core 2 Extreme <45873899+Core-2-Extreme@users.noreply.github.com>
2025-01-18 10:25:00 +0900

    Added get features

--- k11_extension/include/svc/BetterScheduler.h
+++ k11_extension/include/svc/BetterScheduler.h
@@ -30,12 +30,17 @@
 #include "kernel.h"
 #include "svc.h"
 
-#define BETTER_SCHEDULER_START_SCHEDULER    (u32)(0x00040000)
-#define BETTER_SCHEDULER_STOP_SCHEDULER     (u32)(0x00040001)
-#define BETTER_SCHEDULER_DEBUG              (u32)(0x00040002)
-#define BETTER_SCHEDULER_REGISTER_THREAD    (u32)(0x00040005)
-#define BETTER_SCHEDULER_UNREGISTER_THREAD  (u32)(0x00040006)
-#define BETTER_SCHEDULER_SET_AFFINITY_MASK  (u32)(0x00040010)
+#define BETTER_SCHEDULER_START_SCHEDULER    (u32)(0x00040000)   //Start a custom scheduler (it should only be called from luma3ds (rosalina)).
+#define BETTER_SCHEDULER_STOP_SCHEDULER     (u32)(0x00040001)   //Stop a custom scheduler (it should only be called from luma3ds (rosalina)).
+#define BETTER_SCHEDULER_FEATURES           (u32)(0x00040002)   //Read features.
+#define BETTER_SCHEDULER_REGISTER_THREAD    (u32)(0x00040005)   //Register a thread.
+#define BETTER_SCHEDULER_UNREGISTER_THREAD  (u32)(0x00040006)   //Unregister a thread.
+#define BETTER_SCHEDULER_SET_AFFINITY_MASK  (u32)(0x00040010)   //Set thread affinity mask.
+#define BETTER_SCHEDULER_DEBUG              (u32)(0x000400FF)   //Debug.
+
+#define BETTER_SCHEDULER_FEATURE_NONE               (u32)(0x00000000)   //No features.
+#define BETTER_SCHEDULER_FEATURE_CROSS_CORE         (u32)(0x00000001)   //Cross-core context switch is supported.
+#define BETTER_SCHEDULER_FEATURE_CORE_1_UNLIMITED   (u32)(0x00000002)   //Use of core #1 is unlimited.
 
 void BetterSchedulerUpdateInSvcFlag(KThread *thread, bool inSvc);
 void BetterSchedulerRemoveThread(KThread* thread);
